fda93a844b5516c5ff4464f14168e36c899ac822
VictorrDionizio <vicordionizio@gmail.com>
2026-04-01 21:27:43 -0300

    Add files via upload

---
+++ loja.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+    float totalcompra;
+    printf("Digite o total da compra: ");
+    scanf("%f", &totalcompra); 
+    if( totalcompra > 50.0 ) {
+    printf("Parabéns, você ganhou um brinde!");
+}
+
+}
